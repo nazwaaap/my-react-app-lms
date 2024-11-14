ad50b9b979da8fc6eed255584a912ae01dc198c7
nazwaaap <pradittanazwa@gmail.com>
2024-11-14 23:53:52 +0700

    feat: add favorite foods list in react component - 7.2

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+// import App from './App.jsx'
+// import './index.css'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <div>
+    <h1>My Favorite Foods</h1>
+    <ul>
+      <li>Burger</li>
+      <li>Noodles</li>
+    </ul>
+  </div>
 )
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//     <h1>Hello World</h1>
+// )
+
+// var h1 = document.createElement('h1')
+// h1.innerHTML = "Hello World"
+// document.getElementById('root').appendChild(h1)
