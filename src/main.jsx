7f0dfc3c2d1fabe2f15214630a5387ac85180403
nazwaaap <pradittanazwa@gmail.com>
2024-11-15 00:07:09 +0700

    feat: add name to heading and style favorite foods list - 7.3

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,21 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 // import App from './App.jsx'
 // import './index.css'
+import './styles.css'
+
+const myName = "Nazwa Praditta"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <div>
-    <h1>My Favorite Foods</h1>
-    <ul>
-      <li>Burger</li>
-      <li>Noodles</li>
-    </ul>
-  </div>
+  <React.StrictMode>
+    <div>
+      <h1 className="heading">{myName} My Favorite Foods</h1>
+      <ul>
+        <li>Burger</li>
+        <li>Noodles</li>
+        <li>Martabak</li>
+      </ul>
+    </div>
+  </React.StrictMode>,
 )
 
 // ReactDOM.createRoot(document.getElementById('root')).render(
