473de67183f2adc7e4ac9af07cdef6570b2ab436
nazwaaap <pradittanazwa@gmail.com>
2024-11-15 00:21:26 +0700

    feat: add and render Heading & List components - 7.4

---
+++ src/components/List.jsx
@@ -0,0 +1,11 @@
+const List = () => {
+    return (
+        <ul>
+            <li>Burger</li>
+            <li>Noodles</li>
+            <li>Martabak</li>
+        </ul>
+    )
+}
+
+export default List
--- src/main.jsx
+++ src/main.jsx
@@ -2,22 +2,33 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 // import App from './App.jsx'
 // import './index.css'
-import './styles.css'
-
-const myName = "Nazwa Praditta"
+// import './styles.css'
+import Heading from './components/Heading'
+import List from './components/List'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <div>
-      <h1 className="heading">{myName} My Favorite Foods</h1>
-      <ul>
-        <li>Burger</li>
-        <li>Noodles</li>
-        <li>Martabak</li>
-      </ul>
+      <Heading />
+      <List />
     </div>
   </React.StrictMode>,
 )
+
+// const myName = "Nazwa Praditta"
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <div>
+//       <h1 className="heading">{myName} My Favorite Foods</h1>
+//       <ul>
+//         <li>Burger</li>
+//         <li>Noodles</li>
+//         <li>Martabak</li>
+//       </ul>
+//     </div>
+//   </React.StrictMode>,
+// )
 
 // ReactDOM.createRoot(document.getElementById('root')).render(
 //     <h1>Hello World</h1>
