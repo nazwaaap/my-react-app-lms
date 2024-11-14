e8e8630bca4f3884a6cd9d1e5e49ca272d7ce250
nazwaaap <pradittanazwa@gmail.com>
2024-11-15 00:35:48 +0700

    feat: add props card component for fruit display with styling - 7.5

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,66 @@
-import React from 'react'
+import React from 'react';
 import ReactDOM from 'react-dom/client'
-// import App from './App.jsx'
+import './styles.css'
+// import App from './App/jsx'
 // import './index.css'
-// import './styles.css'
-import Heading from './components/Heading'
-import List from './components/List'
+
+function Card(props) {
+  return (
+    <div className='my-style'>
+      <h2>{props.name}</h2>
+      <img
+        src={props.img}
+        alt='fruit_img'
+        style={{width: '150px', height: '150px'}}
+      />
+      <p>Harga: {props.harga}</p>
+      <p>Stok: {props.stok}</p>
+    </div>
+  )
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <div>
-      <Heading />
-      <List />
+      <h1>Toko Buah</h1>
+      <Card
+        name="Apel"
+        img="https://asset.kompas.com/crops/edXTLdy8MyoWURzug5j6nCVhhxM=/21x0:1476x970/750x500/data/photo/2022/08/30/630d7ae5d041f.jpg"
+        harga="25000"
+        stok="15"
+      />
+      <Card
+        name="Jeruk"
+        img="https://faperta.umsu.ac.id/wp-content/uploads/2023/06/Manfaat-Buah-Jeruk-1140x570.png"
+        harga="20000"
+        stok="10"
+      />
+      <Card
+        name="Mangga"
+        img="https://asset.kompas.com/crops/xHpaaT9LBsjqfk0sOAYNwUNNQ8Q=/0x0:1000x667/750x500/data/photo/2023/09/22/650cf91bdc362.jpg"
+        harga="30000"
+        stok="30"
+      />
     </div>
-  </React.StrictMode>,
+  </React.StrictMode>
 )
+
+// import React from 'react'
+// import ReactDOM from 'react-dom/client'
+// // import App from './App.jsx'
+// // import './index.css'
+// // import './styles.css'
+// import Heading from './components/Heading'
+// import List from './components/List'
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <div>
+//       <Heading />
+//       <List />
+//     </div>
+//   </React.StrictMode>,
+// )
 
 // const myName = "Nazwa Praditta"
 
